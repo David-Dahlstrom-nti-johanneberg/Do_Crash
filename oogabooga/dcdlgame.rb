eb545b399b61d77674a3e33317f375e4a43f4fca
David-Dahlstrom-nti-johanneberg <94974938+David-Dahlstrom-nti-johanneberg@users.noreply.github.com>
2022-10-17 09:17:26 +0200

    lander class
    lander is made into a classs

--- oogabooga/dcdlgame.rb
+++ oogabooga/dcdlgame.rb
@@ -1,74 +1,41 @@
 require 'gosu'
+require './lander'
 
 class Dont_crash_do_land < Gosu::Window
    
     def initialize
         super(1024,862)
         self.caption = "dont_crash_do_land 0.1"
-        @Lander = Gosu::Image.new("../media/images/whitebox.png")
-        # position: pixels; velocity: pixels per second;
-        # angle: degrees per second ;acceleration: pixels per second^2
-        @x = self.width/2
-        @y = self.height/2
-        @x_vel = 0.1        # initial velocity
-        @y_vel = 0
-        @angle = 0          # right is positive; left is negative
-        @boosterAcc = 3
-        @gravityAcc = 1    
-    end
-
-    def rotate_left
-        @angle -= (360/100)
-    end
-
-    def rotate_right
-        @angle += (360/100)
-    end
-
-    def accelerate
-
-        @x_vel += (Math.sin(@angle) * @booster)
-        @y_vel -= (Math.cos(@angle) * @booster)
-
+        @lander = Lander.new((self.width/2), (self.height/2))
     end
 
     def update
-
-        rotate_left if Gosu.button_down? Gosu::KB_LEFT
-        rotate_right if Gosu.button_down? Gosu::KB_RIGHT
-        accelerate if Gosu.button_down? Gosu::KB_UP
-
-        if @x >= 1024 - @Lander::width
-            @x = @Lander::width
-        elsif @x <= @Lander::width
-            @x = 1024 - @Lander.width
+        if @lander.x >= self.width - @lander.width
+            @lander.x = @lander.width
+        elsif @lander.x <= @lander.width
+            @lander.x = (self.width - @lander.width)
         end
 
-        if @y >= 862 - @Lander::height
-            @aliveangle = ( @angle <= (360/4) || @angle >= -(360/4) )
+        if @lander.y >= self.height - @lander.height
+            @aliveangle = ( @lander.angle <= (360/4) || @lander.angle >= -(360/4) )
             p @aliveangle
-            if  @aliveangle && @y_vel < 10 && @x_vel < 10
+            if  @aliveangle && @lander.y_vel < 10 && @lander.x_vel < 10
                 puts "mortaza will kiss ur feet as reward"
-                @y_vel = 0
-                @x_vel = 0
+                @lander.y_vel = 0
+                @lander.x_vel = 0
             else
                 puts "u suck"
-                @y_vel = 0
-                @x_vel = 0
-
+                @lander.y_vel = 0
+                @lander.x_vel = 0
             end
         end
 
-        @y_vel += @gravity
-        @x += @x_vel / 60
-        @y += @y_vel / 60
-        
+        @lander.update
     end
 
     def draw
-        @Lander.draw_rot(@x,@y,0,@angle,0.5,0.5,1,1)
+        @lander.draw
     end
-
 end
 
 game = Dont_crash_do_land.new
---
+++ oogabooga/lander.rb
@@ -0,0 +1,48 @@
+require 'gosu'
+
+class Lander
+
+    attr_reader :x, :y, :x_vel, :y_vel, :angle, :width, :height
+
+    def initialize(x,y)
+        @x = x
+        @y = y
+        @image = Gosu::Image.new("../media/images/whitebox.png")
+        @width = @image.width
+        @height = @image.height
+        @x_vel = 0.1        # initial velocity
+        @y_vel = 0
+        @angle = 0          # right is positive; left is negative
+        @boosterAcc = 2
+        @gravityAcc = 1  
+    end
+
+    def rotate_left
+        @angle -= (360/100)
+    end
+
+    def rotate_right
+        @angle += (360/100)
+    end
+
+    def accelerate
+        angle_radians = Gosu::degrees_to_radians(@angle)
+        @x_vel += (Math.sin(angle_radians) * @boosterAcc)
+        @y_vel -= (Math.cos(angle_radians) * @boosterAcc)
+    end
+
+    def update
+        rotate_left if Gosu.button_down? Gosu::KB_LEFT
+        rotate_right if Gosu.button_down? Gosu::KB_RIGHT
+        accelerate if Gosu.button_down? Gosu::KB_UP
+
+        @y_vel += @gravityAcc
+        @x += @x_vel / 60
+        @y += @y_vel / 60
+    end
+
+    def draw
+        @image.draw_rot(@x,@y,0,@angle,0.5,0.5,1,1)
+    end
+
+end
